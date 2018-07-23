dcfd904c3b78f6e2a79dfe73efa87364469b3afa
MarkNewcomb1 <wlmmn@hotmail.com>
2018-07-23 14:19:26 -0600

    silly cats app in Vue during breakout

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header v-bind:cats="cats" v-bind:logCats="logCats"/>
+    <img src="./assets/logo.png" v-on:click="logCats">
+    <HelloWorld/>
+    <h1>HELLOO</h1>
+    <p>This is a paragraph</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Header
+  },
+  data() {
+    return {
+      cats: ['garfield', 'heathcliff', 'hobbes']
+    }
+  },
+  methods: {
+    logCats() {
+      console.log(this.cats);
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<h1 v-on:click="logCats">THIS IS THE HEADER, BRUH</h1>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    props: ['cats', 'logCats']
+}
+</script>
+
+<style scoped>
+
+</style>
